0cab06e6ebedb3f5cf922ffddb52839a964c5b21
kmwalsh211 <kmwalsh211@gmail.com>
2025-09-09 21:44:37 -0400

    changed item class

--- src/main/java/edu/trincoll/tracker/Movie.java
+++ src/main/java/edu/trincoll/tracker/Movie.java
@@ -19,23 +19,25 @@ import java.util.Objects;
  * - Recipe: name, ingredients, instructions, prepTime
  * - Movie: title, director, year, rating, watched
  */
-public class Item {
+public class Movie {
     
     private Long id;
     
     // TODO: Replace these example fields with your domain-specific fields
-    @NotBlank(message = "Name is required")
-    @Size(min = 1, max = 100, message = "Name must be between 1 and 100 characters")
-    private String name;
+    @NotBlank(message = "Title is required")
+    @Size(min = 1, max = 100, message = "Title must be between 1 and 100 characters")
     
     private String description;
-    
     private LocalDateTime createdAt;
-    
     private boolean completed;
+    private String title;
+    private String director;
+    private int year;
+    private int rating;
+    private boolean watched;
     
     // Constructor
-    public Item() {
+    public Movie() {
         this.createdAt = LocalDateTime.now();
         this.completed = false;
     }
@@ -51,18 +53,30 @@ public class Item {
         this.id = id;
     }
     
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
     public String getDescription() {
         return description;
     }
-    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
     public void setDescription(String description) {
         this.description = description;
     }
@@ -82,30 +96,50 @@ public class Item {
     public void setCompleted(boolean completed) {
         this.completed = completed;
     }
-    
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
     // TODO: Consider overriding equals() and hashCode() based on your domain
     // This is important for testing and collections
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return completed == item.completed &&
-               Objects.equals(id, item.id) &&
-               Objects.equals(name, item.name) &&
-               Objects.equals(description, item.description);
+        Movie movie = (Movie) o;
+        return completed == movie.completed &&
+               Objects.equals(id, movie.id) &&
+               Objects.equals(title, movie.title) &&
+               Objects.equals(description, movie.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, description, completed);
+        return Objects.hash(id, title, description, completed);
     }
 
     @Override
     public String toString() {
         return "Item{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", name='" + title + '\'' +
                 ", description='" + description + '\'' +
                 ", createdAt=" + createdAt +
                 ", completed=" + completed +
